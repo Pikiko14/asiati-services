b41525e5fb40ad29872939a5935c46051c691fd4
olasanmi <maalhequi95@gmail.com>
2024-05-06 10:15:40 -0500

    initital commit

---
+++ auth/src/interfaces/users.interface.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { LoginInterface } from "./auth.interface";
+
+export enum UserRole {
+  ADMIN = "admin",
+  EMPLOYEE = "employee",
+  WHOLESALE = "whole_sale",
+  CLIENT = "client",
+}
+
+export interface User extends LoginInterface {
+  id?: string;
+  _id?: string;
+  email: string;
+  role?: UserRole;
+  scopes?: string[];
+  is_active?: boolean;
+  recovery_token?: string;
+  profile_pictury?: string;
+  name: string | undefined;
+  token?: string | undefined;
+  last_name: string | undefined;
+  confirmation_token?: string | null;
+}
