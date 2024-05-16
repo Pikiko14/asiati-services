015c38b11a8ebbabd00ae6a1e3628e28eecf1ea6
olasanmi <maalhequi95@gmail.com>
2024-05-16 09:45:40 -0500

    fix validator auth

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -4,77 +4,77 @@ import { Request, Response, NextFunction } from "express";
 import UserRepository from "../repositories/user.repository";
 import { handlerValidator } from "../utils/handler.validator";
 
-// instanciate all class neccesaries
+// Instancia todas las clases necesarias
 const utils = new Utils();
 const repository = new UserRepository();
 
-// build validators
+// Construye los validadores
 
 const RegisterValidator = [
   check("username")
     .exists()
-    .withMessage("Username does not exist")
+    .withMessage("El campo username es requerido")
     .notEmpty()
-    .withMessage("Username is empty")
+    .withMessage("El campo usuario no puede estar vacío")
     .isString()
-    .withMessage("Username must be a string")
+    .withMessage("El campo username debe ser un string")
     .isLength({ min: 5, max: 90 })
-    .withMessage("Username must have a minimum of 5 characters")
+    .withMessage("El username debe tener entre 5 y 90 caracteres")
     .custom(async (username: string) => {
       const existUser = await repository.getUserByUsername(username);
       if (existUser) {
-        throw new Error("Username exist in our records");
+        throw new Error("El username ya existe en nuestra base de datos");
       }
       return true;
     }),
   check("password")
     .exists()
-    .withMessage("Password is empty")
+    .withMessage("La contraseña es requerida")
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long")
+    .withMessage("La contraseña debe tener al menos 8 caracteres")
     .matches(/[A-Z]/)
-    .withMessage("Password must contain at least one uppercase letter")
+    .withMessage("La contraseña debe tener al menos una mayúscula")
     .matches(/[a-z]/)
-    .withMessage("Password must contain at least one lowercase letter")
+    .withMessage("La contraseña debe tener al menos una minúscula")
     .matches(/\d/)
-    .withMessage("Password must contain at least one number")
+    .withMessage("La contraseña debe tener al menos un número")
     .matches(/[$@#&!*-]/)
     .withMessage(
-      "Password must contain at least one special character like $, @, #, &, - or !"
+      "La contraseña debe tener al menos un caracter especial como $, @, #, &, - o !"
     ),
   check("name")
     .exists()
-    .withMessage("name does not exist")
+    .withMessage("El nombre no existe")
     .notEmpty()
-    .withMessage("name is empty")
+    .withMessage("El nombre está vacío")
     .isString()
-    .withMessage("name must be a string")
+    .withMessage("El nombre debe ser un string")
     .isLength({ min: 4, max: 90 })
-    .withMessage("name must have a minimum of 4 characters and maximum 90"),
+    .withMessage("El nombre debe tener un mínimo de 4 caracteres y máximo 90"),
   check("last_name")
     .exists()
-    .withMessage("Last name dost not exist")
+    .withMessage("El apellido no existe")
     .notEmpty()
-    .withMessage("Last name is empty")
+    .withMessage("El apellido está vacío")
     .isString()
-    .withMessage("Last name must be a string")
+    .withMessage("El apellido debe ser un string")
     .isLength({ min: 3, max: 90 })
-    .withMessage("Last name must have a minimum of 3 characters"),
+    .withMessage("El apellido debe tener un mínimo de 3 caracteres"),
   check("email")
     .exists()
-    .withMessage("Email does not exist")
+    .withMessage("El correo electrónico no existe")
     .notEmpty()
-    .withMessage("Email is empty")
+    .withMessage("El correo electrónico está vacío")
     .isString()
-    .withMessage("Email must be a string")
+    .withMessage("El correo electrónico debe ser un string")
     .isEmail()
-    .withMessage("Invalid email format")
+    .withMessage("Formato de correo electrónico inválido")
     .isLength({ min: 5, max: 90 })
-    .withMessage("Email must have a minimum of 5 characters")
+    .withMessage("El correo electrónico debe tener un mínimo de 5 caracteres")
     .custom(async (email: string) => {
       const existEmail = await repository.getUserByEmail(email);
       if (existEmail) {
-        throw new Error("Email exist in our records");
+        throw new Error("El correo electrónico ya existe en nuestros registros");
       }
       return true;
     }),
@@ -89,22 +89,22 @@ const RegisterValidator = [
 const ConfirmationUserValidator = [
   check("action")
     .exists()
-    .withMessage("Action is required")
+    .withMessage("La acción es requerida")
     .notEmpty()
-    .withMessage("Action is empty")
+    .withMessage("La acción está vacía")
     .isString()
-    .withMessage("Action must be a string"),
+    .withMessage("La acción debe ser un string"),
   check("token")
     .exists()
-    .withMessage("Token is required")
+    .withMessage("El token es requerido")
     .notEmpty()
-    .withMessage("Token is empty")
+    .withMessage("El token está vacío")
     .isString()
-    .withMessage("Token must be a string")
+    .withMessage("El token debe ser un string")
     .custom(async (token: string) => {
       const existToken = await repository.getUserByToken(token);
       if (!existToken) {
-        throw new Error("Token don't exist in our records");
+        throw new Error("El token no existe en nuestros registros");
       }
       return true;
     }),
@@ -115,39 +115,39 @@ const ConfirmationUserValidator = [
 const LoginValidator = [
   check("username")
     .exists()
-    .withMessage("Username does not exist")
+    .withMessage("El nombre de usuario no existe")
     .notEmpty()
-    .withMessage("Username is empty")
+    .withMessage("El nombre de usuario está vacío")
     .isString()
-    .withMessage("Username must be a string")
+    .withMessage("El nombre de usuario debe ser un string")
     .isLength({ min: 5, max: 90 })
-    .withMessage("Username must have a minimum of 5 characters")
+    .withMessage("El nombre de usuario debe tener un mínimo de 5 caracteres")
     .custom(async (username: string) => {
       const existUser = await repository.getUserByUsername(username);
-      // validate if user don't exist
+      // Validar si el usuario no existe
       if (!existUser) {
-        throw new Error("Username don't exist in our records");
+        throw new Error("El nombre de usuario no existe en nuestros registros");
       }
-      // validate if user is ! active
+      // Validar si el usuario no está activo
       if (existUser && !existUser.is_active) {
-        throw new Error("User is not active");
+        throw new Error("El usuario no está activo");
       }
       return true;
     }),
   check("password")
     .exists()
-    .withMessage("Password is empty")
+    .withMessage("La contraseña está vacía")
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long")
+    .withMessage("La contraseña debe tener al menos 8 caracteres")
     .matches(/[A-Z]/)
-    .withMessage("Password must contain at least one uppercase letter")
+    .withMessage("La contraseña debe contener al menos una letra mayúscula")
     .matches(/[a-z]/)
-    .withMessage("Password must contain at least one lowercase letter")
+    .withMessage("La contraseña debe contener al menos una letra minúscula")
     .matches(/\d/)
-    .withMessage("Password must contain at least one number")
+    .withMessage("La contraseña debe contener al menos un número")
     .matches(/[$@#&!*-]/)
     .withMessage(
-      "Password must contain at least one special character like $, @, #, &, - or !"
+      "La contraseña debe contener al menos un carácter especial como $, @, #, &, - o !"
     ),
   (req: Request, res: Response, next: NextFunction) =>
     handlerValidator(req, res, next),
@@ -156,19 +156,19 @@ const LoginValidator = [
 const RecoveryValidator = [
   check("email")
     .exists()
-    .withMessage("Email does not exist")
+    .withMessage("El correo electrónico no existe")
     .notEmpty()
-    .withMessage("Email is empty")
+    .withMessage("El correo electrónico está vacío")
     .isString()
-    .withMessage("Email must be a string")
+    .withMessage("El correo electrónico debe ser un string")
     .isEmail()
-    .withMessage("Invalid email format")
+    .withMessage("Formato de correo electrónico inválido")
     .isLength({ min: 5, max: 90 })
-    .withMessage("Email must have a minimum of 5 characters")
+    .withMessage("El correo electrónico debe tener un mínimo de 5 caracteres")
     .custom(async (email: string) => {
       const existEmail = await repository.getUserByEmail(email);
       if (!existEmail) {
-        throw new Error("Email don't exist in our records");
+        throw new Error("El correo electrónico no existe en nuestros registros");
       }
       return true;
     }),
@@ -179,54 +179,54 @@ const RecoveryValidator = [
 const ChangePasswordValidator = [
   check("password")
     .exists()
-    .withMessage("Password is empty")
+    .withMessage("La contraseña está vacía")
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long")
+    .withMessage("La contraseña debe tener al menos 8 caracteres")
     .matches(/[A-Z]/)
-    .withMessage("Password must contain at least one uppercase letter")
+    .withMessage("La contraseña debe contener al menos una letra mayúscula")
     .matches(/[a-z]/)
-    .withMessage("Password must contain at least one lowercase letter")
+    .withMessage("La contraseña debe contener al menos una letra minúscula")
     .matches(/\d/)
-    .withMessage("Password must contain at least one number")
+    .withMessage("La contraseña debe contener al menos un número")
     .matches(/[$@#&!*-]/)
     .withMessage(
-      "Password must contain at least one special character like $, @, #, &, - or !"
+      "La contraseña debe contener al menos un carácter especial como $, @, #, &, - o !"
     ),
   check("confirmation_password")
     .exists()
-    .withMessage("Password is empty")
+    .withMessage("La contraseña está vacía")
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long")
+    .withMessage("La contraseña debe tener al menos 8 caracteres")
     .matches(/[A-Z]/)
-    .withMessage("Password must contain at least one uppercase letter")
+    .withMessage("La contraseña debe contener al menos una letra mayúscula")
     .matches(/[a-z]/)
-    .withMessage("Password must contain at least one lowercase letter")
+    .withMessage("La contraseña debe contener al menos una letra minúscula")
     .matches(/\d/)
-    .withMessage("Password must contain at least one number")
+    .withMessage("La contraseña debe contener al menos un número")
     .matches(/[$@#&!*-]/)
     .withMessage(
-      "Password must contain at least one special character like $, @, #, &, - or !"
+      "La contraseña debe contener al menos un carácter especial como $, @, #, &, - o !"
     )
     .custom((val: string, { req }) => {
       if (val !== req.body?.password) {
-        throw new Error("Password don´t match");
+        throw new Error("Las contraseñas no coinciden");
       }
       return true;
     }),
   check("token")
     .exists()
-    .withMessage("Token is empty")
+    .withMessage("El token está vacío")
     .custom(async (token: string) => {
       const existToken = await repository.getUserByToken(token);
       if (!existToken) {
-        throw new Error("Token don't exist in our records");
+        throw new Error("El token no existe en nuestros registros");
       }
       return true;
     })
     .custom(async (token: string) => {
       const isValid = await utils.verifyToken(token);
       if (!isValid) {
-        throw new Error("Token expired");
+        throw new Error("El token ha caducado");
       }
       return true;
     }),
